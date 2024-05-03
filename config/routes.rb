90718ae0d700f5119385fe9e6e13a6d2914459a8
Lucie Oken <lucie.oken@kellogg.northwestern.edu>
2024-05-03 15:55:46 +0000

    Updated dice, added companies

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/tacos", :controller => "tacos", :action => "index")
   get("/dice", :controller => "dice", :action => "index")
+  # resources "companies" is the equivalent to the below 
+  get("/companies", :controller => "companies", :action => "index")
 end
